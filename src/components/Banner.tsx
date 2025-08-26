7d5adf9bc7095c50821a85ddf96a56c52b6e1f76
sleepielle <pggisselle@gmail.com>
2025-08-26 00:49:57 -0600

    feat:implement navbar component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,11 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,214,255,0.7),rgb(41,216,255,0.7),rgb(255,253,128,0.7),rgb(248,154,191,0.7),rgb(252,214,255,0.7))]">
+      <div className="container">
+        <a href="#" className="underline underline-offset-4 font-medium">
+          Explore the demo
+        </a>
+      </div>
+    </div>
+  );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,60 @@
+import Image from "next/image";
+import Logo from "../assets/images/logosaas.png";
+import MenuIcon from "../assets/icons/menu.svg";
+
 export const Navbar = () => {
-  return null;
+  return (
+    <div className="bg-black">
+      <div className="px-4">
+        <div className="py-4 flex items-center justify-between">
+          <div className="relative">
+            <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,rgb(252,214,255,0.7),rgb(41,216,255,0.7),rgb(255,253,128,0.7),rgb(248,154,191,0.7),rgb(252,214,255,0.7))] blur-md"></div>
+            <Image src={Logo} alt="saas-logo" className="h-12 w-12 relative" />
+          </div>
+
+          <div className=" inline-flex justify-center items-center rounded-lg md:hidden">
+            <button className="bg-white py-2 px-4 rounded-lg">
+              Get for free
+            </button>
+          </div>
+
+          <nav className=" items-center gap-6 hidden sm:flex">
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Updates
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Help
+            </a>
+            <a
+              href="#"
+              className="text-opacity-60 text-white hover:text-opacity-100 transition"
+            >
+              Customers
+            </a>
+            <button className="bg-white py-2 px-4 rounded-lg">
+              Get for free
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
