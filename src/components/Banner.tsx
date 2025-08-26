830ab1b4d81309b2e74c07004d54287cd9faa454
sleepielle <pggisselle@gmail.com>
2025-08-26 05:05:04 -0600

    feat: implemented logo ticker section. pending animation.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,11 @@ export const Banner = () => {
   return (
     <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,214,255,0.7),rgb(41,216,255,0.7),rgb(255,253,128,0.7),rgb(248,154,191,0.7),rgb(252,214,255,0.7))]">
       <div className="container">
+        <span className="font-medium">
+          Introducing a complete redesigned interface -{" "}
+        </span>
         <a href="#" className="underline underline-offset-4 font-medium">
-          Explore the demo
+          <span>Explore the demo</span>
         </a>
       </div>
     </div>
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
@@ -15,5 +16,35 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black py-[72px]">
+      <div className="container">
+        <div className="flex items-center justify-center flex-col pt-8 ">
+          <h2 className="text-white/70 text-center m-8 text-xl">
+            Trusted by the worlds most innovative teams
+          </h2>
+          <div
+            className="
+            relative overflow-hidden mt-9
+            before:content-[''] before:absolute before:inset-y-0 before:left-0 before:w-5
+            before:bg-gradient-to-r before:from-black before:to-black/0
+            after:content-[''] after:absolute after:inset-y-0 after:right-0 after:w-5
+            after:bg-gradient-to-l after:from-black after:to-black/0
+          "
+          >
+            <div className="flex gap-16">
+              {images.map((logo) => (
+                <Image
+                  src={logo.src}
+                  alt={logo.alt}
+                  key={logo.alt}
+                  className="flex-none h-8 w-auto"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
